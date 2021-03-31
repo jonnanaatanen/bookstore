c8519079135c68bb879922c6c7c41ce00b83f91a
jonnanaatanen <jonna.naatanen@hotmail.com>
2021-03-31 21:32:33 +0300

    Initial commit

--- bookstore/src/main/java/fi/hh/swd20/bookstore/BookstoreApplication.java
+++ bookstore/src/main/java/fi/hh/swd20/bookstore/BookstoreApplication.java
@@ -45,7 +45,7 @@ public class BookstoreApplication {
 	repository.save(b2);
 	repository.save(b3);
 	
-	// Create users: admin/admin user/user
+	// Create users: 
 	 User user1 = new User("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "user@user.com", "USER");
 	 User user2 = new User("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C", "admin@admin.com", "ADMIN");
 	 urepository.save(user1);
